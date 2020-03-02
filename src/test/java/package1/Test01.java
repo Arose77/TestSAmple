e6d6100ff7f0c3576f7bf072156682daef320b3f
arose77 <ammurose45@gmail.com>
2020-03-02 12:13:11 +0530

    Committing testingproject1

---
+++ src/test/java/package1/Test01.java
@@ -0,0 +1,11 @@
+package package1;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("testing 01 package");
+		System.out.println("testing project");
+	}
+
+}
